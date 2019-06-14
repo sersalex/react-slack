0bbe7438329beb5b48c16b4ff4b407482834d07d
Sergey Alexeev <sersalex@yandex.ru>
2019-06-14 21:26:12 +0300

    add validation form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,17 +1,57 @@
 import React from 'react'
+import firebase from '../../firebase'
+
 import {Grid, Header, Icon, GridColumn, Form, Segment, FormInput, Button, Message} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 class Register extends React.Component {
   state = {
-
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+    errors: []
   }
 
   handleChange = event => {
     this.setState({[event.target.name]: event.target.value})
   };
 
+  isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (this.isFormEmpty(this.state)) {
+      error = { message: 'Fill in all fields' };
+      this.setState({errors: errors.concat(error)})
+      return false;
+      // throw error
+    } else if (!this.isPasswordValid()) {
+      // throw error
+    } else {
+      return true
+    }
+  }
+
+  isFormEmpty = ({username, email, password, passwordConfirmation}) => {
+    return !username.length || !email.length || !password.length || !passwordConfirmation.length
+  }
+
+  isPasswordValid = () => {}
+
+  handleSubmit = event => {
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(user => console.log(user))
+        .catch(err => console.log(err))
+    }
+  }
+
   render() {
+    const {username, email, password, passwordConfirmation} = this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <GridColumn style={{maxWidth: 450}}>
@@ -19,7 +59,7 @@ class Register extends React.Component {
             <Icon name="puzzle piece" color="orange"/>
             Register for DevChat
           </Header>
-          <Form size="large">
+          <Form onSubmit={this.handleSubmit} size="large">
             <Segment stacked>
               <FormInput
                 fluid
@@ -28,15 +68,17 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Username"
                 type="text"
+                value={username}
                 onChange={this.handleChange}
               />
               <FormInput
                 fluid
                 name="email"
-                icon="email"
+                icon="mail"
                 iconPosition="left"
                 placeholder="Email Address"
                 type="email"
+                value={email}
                 onChange={this.handleChange}
               />
               <FormInput
@@ -46,6 +88,7 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Password"
                 type="password"
+                value={password}
                 onChange={this.handleChange}
               />
               <FormInput
@@ -55,6 +98,7 @@ class Register extends React.Component {
                 iconPosition="left"
                 placeholder="Password Confirmation"
                 type="password"
+                value={passwordConfirmation}
                 onChange={this.handleChange}
               />
 
